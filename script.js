13baa11057bcff0e6e95b44bb366010bde1daad9
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-07-25 12:04:49 -0500

    Add mobil version main page

---
+++ script.js
@@ -0,0 +1,20 @@
+// Hamburguer button works
+const modal = document.querySelector('#modal-menu');
+const hambIcon = document.querySelector('#burguer-icon');
+const xIcon = document.querySelector('#x-icon');
+const list = document.querySelector('#list-menu');
+
+// Open de hamburguer menu
+hambIcon.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+// Close de hamburguer menu
+xIcon.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+// If click in an option from de memu, close the menu
+list.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
